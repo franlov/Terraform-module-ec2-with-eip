56fc55259341f3596fc74955239117bec297d80e
Francisco Leon <francisco.leong@gmail.com>
2022-09-14 12:49:15 +0200

    Agregado modulo para ec2-eip

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ec2-With-Eip - Terraform Module 
+
+* Crea una Instancia - ubuntu desde un Data-source (Puedes modificar los datos de la instancia a crear)
+* Crea una Elastic IP asociada a la instancia
+* Crea una keypar asociada a la instancia, para autentificarse con ella por SSH (Requiere el modulo Keypar)
+* Crea un grupo de seguridad asociado a la instancia con todos los puertos abiertos.
---
+++ instance.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "web" {
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  instance_type          = "${var.instance_type}"
+  vpc_security_group_ids = ["${aws_security_group.sg.id}"]
+  key_name               = "${var.project_name}-${var.environment}"
+  
+  tags = {
+    Name = "${var.project_name}-${var.environment}"
+  }
+}
+
+module "keypair" { 
+  source = "https://github.com/franlov/Terraform-modules/"
+  project_name = "${var.project_name}"
+  environment = "${var.environment}"
+  key_name = "${var.project_name}-${var.environment}"
+
+}
