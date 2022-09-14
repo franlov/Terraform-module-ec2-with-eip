bfa6f2c9419f11ba3d83a5763e2a10766d0e0016
Francisco Leon <francisco.leong@gmail.com>
2022-09-14 13:40:32 +0200

    Se modifica el modulo a local

--- instance.tf
+++ instance.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "web" {
 }
 
 module "keypair" { 
-  source = "https://github.com/franlov/Terraform-module-keypair"
+  source = "/home/franleongt/workspace/terraform/modulos/keypair"
   project_name = "${var.project_name}"
   environment = "${var.environment}"
   key_name = "${var.project_name}-${var.environment}"
